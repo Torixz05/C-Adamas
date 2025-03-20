f05eff08dfef16aac45836ca5bb55202f9c18e82
Torixz05 <torichakraborty92@gmail.com>
2025-03-20 12:16:43 +0530

    Add files via upload

---
+++ V&C.T.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+	char x,a,e,i,o,u;
+	printf("enter x in the lower case:");
+	scanf("%c",&x);
+	
+	if(x=='a')
+	{
+		printf("vowel");
+	}
+	else if(x=='e')
+	{
+		printf("vowel");
+	}
+	else if(x=='i')
+	{
+		printf("vowel");
+	}
+	else if(x=='o')
+	{
+		printf("vowel");
+	}
+	else if(x=='u')
+	{
+		printf("vowel");
+	}
+	else 
+	{
+		printf("consonent");
+	}
+	return 0;
+	
+	
+}
